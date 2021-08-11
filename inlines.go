91fe0b006e96888936d2c3bf5d52cdef52c8c771
Harley Denham <45082543+qxoko@users.noreply.github.com>
2021-08-11 09:23:54 +0100

    tentatively change regex matching on inlines

--- inlines.go
+++ inlines.go
@@ -10,10 +10,10 @@ import (
 // reminder to replace it with something
 // faster
 
-var strike  = regexp.MustCompile(`~([^><]+?)~`)
-var italics = regexp.MustCompile(`\*([^><]+?)\*`)
-var bold    = regexp.MustCompile(`\*\*([^><]+?)\*\*`)
-var link    = regexp.MustCompile(`\[([^><]+?)\]\((.+?)\)`)
+var strike  = regexp.MustCompile(`~(.+?)~`)
+var italics = regexp.MustCompile(`\*(.+?)\*`)
+var bold    = regexp.MustCompile(`\*\*(.+?)\*\*`)
+var link    = regexp.MustCompile(`\[(.+?)\]\((.+?)\)`)
 var inline  = regexp.MustCompile(`c\.(.+?){(.+?)}`)
 
 var code = regexp.MustCompile("`(.+?)`")
