5b3f48277b74f713544907c414d3652db18c78d9
Harley Denham <45082543+qxoko@users.noreply.github.com>
2021-08-12 20:40:57 +0100

    remove scrollback rewriting — it fails more than it succeeds

--- console.go
+++ console.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 type console struct {
-	last_len int
 	scrollback []string
 }
 
@@ -11,17 +10,7 @@ func (e *console) flush() {
 	if len(e.scrollback) == 0 {
 		return
 	}
-
-	for i := 0; i < e.last_len; i++ {
-		fmt.Printf("\033[F\033[K")
-	}
-
-	for _, x := range e.scrollback {
-		fmt.Println(x)
-	}
-
-	e.last_len   = len(e.scrollback)
-	e.scrollback = make([]string, 0, e.last_len)
+	e.scrollback = make([]string, 0, len(e.scrollback))
 }
 
 func (e *console) print(base string, args ...interface{}) {
